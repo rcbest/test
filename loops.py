7d405d710d4e6bab98ebaa1909f3813f68063152
rcserveradm@gmail.com <rcserveradm@gmail.com>
2022-06-20 00:08:53 +0300

    Loopls

---
+++ loops.py
@@ -0,0 +1,19 @@
+x = 0
+while x != 100:
+    print(x)
+    x = x+1
+print("==============================")
+x = 0
+for x in range(0,100,2):
+    print(x)
+cities = list()
+cities = ['Kiev', 'Slavuta', 'truskavetz']
+print(cities[2].title())
+print (len(cities))
+cities.insert(3,'Kvitneve')
+cities.append('Lviv')
+print(cities)
+del cities[3]
+print(cities)
+
+    
